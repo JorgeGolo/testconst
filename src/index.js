fc71dd335793ada0732f81a49c0d656a71b6956d
Jorge Gómez López <jorgelogomez@gmail.com>
2024-11-09 17:43:57 +0100

    added routes 3

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Temas from './Temas';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +12,6 @@ root.render(
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/temas" element={<Temas />} />
-
     </Routes>
   </Router>
 );
