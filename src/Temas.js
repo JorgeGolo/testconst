5abe491d59c5ed991ebf97f7bc7d31fd8d23b7fc
Jorge Gómez López <jorgelogomez@gmail.com>
2024-11-09 12:58:43 +0100

    Nav

---
+++ src/Temas.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+import Nav from './Nav';
+
+const Temas = () => {
+
+
+  return (
+    <div>
+      <Nav/>
+</div>
+  );
+};
+
+export default Temas;
