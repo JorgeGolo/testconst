caf478cfbdf2ab480fab32b79ab04943f63e19ba
Jorge Gómez López <jorgelogomez@gmail.com>
2024-11-09 17:53:48 +0100

    Documents name

--- src/Temas.js
+++ src/Temas.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Nav from './Nav';
 
 const Temas = () => {
-    return (
-        <Nav/>
-    );
-  };
+  const [documentNames, setDocumentNames] = useState([]);
+
+  useEffect(() => {
+    // Función para obtener los nombres de los documentos desde la API
+    const fetchDocumentNames = async () => {
+      try {
+        const response = await fetch('/api/getDocumentNames'); // Ruta de la API
+        const data = await response.json();
+        setDocumentNames(data.documentNames);
+      } catch (error) {
+        console.error('Error al obtener los nombres de los documentos:', error);
+      }
+    };
+
+    fetchDocumentNames();
+  }, []);
+
+  return (
+    <div>
+      <Nav />
+      <h2>Temas</h2>
+      <ul>
+        {documentNames.map((name, index) => (
+          <li key={index}>{name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default Temas;
