6dea1c237c452893d58fa5bcfd5924d439246018
Jorge Gómez López <jorgelogomez@gmail.com>
2024-11-10 11:53:22 +0100

    new collection 2

--- src/Temas.js
+++ src/Temas.js
@@ -2,67 +2,11 @@ import React, { useEffect, useState } from 'react';
 import Nav from './Nav';
 
 const Temas = () => {
-  const [documentNames, setDocumentNames] = useState([]);
-  const [titulo, setTitulo] = useState('');
-  const [capitulo, setCapitulo] = useState('');
-  const [seccion, setSeccion] = useState('');
-  const [articulo, setArticulo] = useState('');
-  const [contenido, setContenido] = useState('');
-  const [respuestaIA, setRespuestaIA] = useState('');
-
-  useEffect(() => {
-    const fetchDocumentNames = async () => {
-      try {
-        const response = await fetch('/api/getDocumentNames');
-        const data = await response.json();
-        setDocumentNames(data.documentNames);
-      } catch (error) {
-        console.error('Error al obtener los nombres de los documentos:', error);
-      }
-    };
-
-    fetchDocumentNames();
-  }, []);
-
-  const obtenerArticulo = async (nombreDocumento) => {
-    console.log("Nombre del documento:", nombreDocumento);
-    try {
-        const response2 = await fetch(`/api/getArticleByCap?documentName=${nombreDocumento}`); // Llamada a la API con el parámetro documentName
-        const data2 = await response2.json();
-
-      if (response2.ok) {
-        setTitulo(data2.titulo);
-        setCapitulo(data2.capitulo);
-        setSeccion(data2.seccion);
-        setArticulo(data2.articulo);
-        setContenido(data2.contenido);
-        setRespuestaIA(data2.respuestaIA);
-      } else {
-        console.error('Error al obtener los datos:', data2.error);
-      }
-    } catch (error) {
-      console.error('Error al obtener los datos:', error);
-    }
-  };
 
   return (
     <div>
       <Nav />
       <h2>Temas</h2>
-      <ul>
-        {documentNames.map((name, index) => (
-          <li key={index}>
-            {name} - <button onClick={() => obtenerArticulo(name)}>Obtener Artículo</button>
-          </li>
-        ))}
-      </ul>
-      {/* Aquí podrías mostrar los datos obtenidos */}
-      <p>{titulo}</p>
-      {capitulo !== 'No aplica' && <p>{capitulo}</p>}
-      {seccion !== 'No aplica' && <p>{seccion}</p>}
-      <p>{articulo}</p>
-      <div dangerouslySetInnerHTML={{ __html: contenido }} />   
-      <p>{respuestaIA}</p>
     </div>
   );
 };
