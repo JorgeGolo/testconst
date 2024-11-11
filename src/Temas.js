d5401bda0b4b21410836126558d1564387c7a46b
Jorge Gómez López <jorgelogomez@gmail.com>
2024-11-11 12:34:28 +0100

    list

--- src/Temas.js
+++ src/Temas.js
@@ -3,9 +3,22 @@ import Nav from './Nav';
 
 const Temas = () => {
 
+
+  const startTitleTest = (title) => {
+    console.log(title);
+  };
+
   return (
     <div>
       <h2>Temas</h2>
+      <div>
+        <ul>
+          <li>Preámbulo</li>
+          <li>Título Preliminar - <button onClick={() => startTitleTest("Título Preliminar")}>Test</button></li>
+          <li>Título I</li>
+          <li>Título II</li>
+        </ul> 
+      </div>
       <div className="subnav">
         <Nav />
       </div>
