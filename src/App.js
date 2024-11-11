95738a385a24585ea053a8d566ae9f65ced97405
Jorge Gómez López <jorgelogomez@gmail.com>
2024-11-11 09:09:02 +0100

    confetti

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Nav from './Nav';
+import Confetti from 'react-confetti';
 
 function App() {
   const [titulo, setTitulo] = useState(null);
@@ -11,11 +12,12 @@ function App() {
   const [opciones, setOpciones] = useState([]);
   const [respuestaCorrecta, setRespuestaCorrecta] = useState(null);
   const [mensaje, setMensaje] = useState('');
+  const [showConfetti, setShowConfetti] = useState(false);
 
   useEffect(() => {
     const obtenerConsultaAleatoria = async () => {
       try {
-        const response = await fetch('/api/getData'); // Llamada a la API
+        const response = await fetch('/api/getData');
         const data = await response.json();
 
         if (response.ok) {
@@ -29,7 +31,7 @@ function App() {
           const lines = data.respuestaIA.split('\n').filter(line => line.trim() !== '');
           setPregunta(lines[0]);
           setOpciones(lines.slice(1, 5));
-          setRespuestaCorrecta(parseInt(lines[5]) - 1); // La respuesta correcta es el índice (0-3)
+          setRespuestaCorrecta(parseInt(lines[5]) - 1);
 
         } else {
           console.error('Error al obtener los datos:', data.error);
@@ -45,21 +47,31 @@ function App() {
   const handleOptionSelect = (index) => {
     if (index === respuestaCorrecta) {
       setMensaje('Respuesta correcta');
+      setShowConfetti(true); // Activa el confeti cuando la respuesta es correcta
+      setTimeout(() => setShowConfetti(false), 3000); // Desactiva el confeti después de 3 segundos
     } else {
       setMensaje('Respuesta incorrecta');
+      setShowConfetti(false);
     }
   };
 
   return (
     <div>
       <Nav/>
-      <p>{mensaje === 'Respuesta correcta' ? (
+      {showConfetti && <Confetti />}
+      <p>
+        {mensaje === 'Respuesta correcta' ? (
           <span style={{ color: 'green' }}>{mensaje}</span>
         ) : mensaje === 'Respuesta incorrecta' ? (
           <span style={{ color: 'red' }}>{mensaje}</span>
-        ) : null
-      }</p>
-
+        ) : null}
+      </p>
+      <hr/>
+      {titulo && <p>{titulo}</p>}
+      {capitulo && <p>Capítulo {capitulo}</p>}
+      {seccion && <p>Sección {seccion}</p>}
+      <p>{articulo}</p>
+      <div dangerouslySetInnerHTML={{ __html: contenido }} />
 
       {pregunta && (
         <div>
@@ -79,13 +91,6 @@ function App() {
           </form>
         </div>
       )}
-
-<hr/>
-      {titulo && <p>{titulo}</p>}
-      {capitulo && <p>Capítulo {capitulo}</p>}
-      {seccion && <p>Sección {seccion}</p>}
-      <p>{articulo}</p>
-      <div dangerouslySetInnerHTML={{ __html: contenido }} />
     </div>
   );
 }
