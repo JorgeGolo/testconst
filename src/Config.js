40b13a265e4dacfc3121fd37b3e6a994bdada431
Jorge Gómez López <jorgelogomez@gmail.com>
2024-11-13 09:39:11 +0100

    Config

--- src/Config.js
+++ src/Config.js
@@ -1,12 +1,34 @@
 import React, { useEffect, useState } from 'react';
 import Nav from './Nav';
+import Switch from "react-switch";
 
 function Config() {
     
-return (
+    const [checkedc, setCheckedc] = useState(false);
 
+
+return (
+    <div className='configcontent'>
+    <ul>
+      <li>
+        Confetti  <Switch 
+        onChange={setCheckedc} 
+        checked={checkedc} 
+        offColor="#888" 
+        onColor="#0f0" 
+      />
+      <p>{checked ? "On" : "Off"}</p>
+      </li>
+      <li>
+        Siguiente pregunta automática
+      </li>
+      <li>
+        Contador de aciertos
+      </li>
+    </ul>
     <div className="subnav">
         <Nav />
+    </div>
     </div>
 );
 
--- src/Nav.js
+++ src/Nav.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Nav = () => {
+    
     return (
+
       <nav className="nav">
-      <Link to="/">Inicio</Link>
-      <Link to="/temas">Temas</Link>
-      <Link to="/config">Temas</Link>
+        <Link to="/">Inicio</Link>
+        <Link to="/temas">Temas</Link>
+        <Link to="/config">Configuración</Link>
 
       </nav>
     );
