75dd68d03c0c5e2edda34d5ad8c1c9508df58687
Jorge Gómez López <jorgelogomez@gmail.com>
2024-11-15 14:14:48 +0100

    new home

--- src/Nav.js
+++ src/Nav.js
@@ -6,8 +6,7 @@ const Nav = () => {
     return (
 
       <nav className="nav">
-        <Link to="/">Inicio</Link>
-        <Link to="/temas">Bloque 1</Link>
+        <Link to="/">Bloque 1</Link>
         <Link to="/config">Configuración</Link>
 
       </nav>
