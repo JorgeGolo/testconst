357708486b435521a83f81f11f27367c5dbfd6ae
Jorge Gómez López <jorgelogomez@gmail.com>
2025-03-01 14:20:31 +0100

    adding API - Notion

--- src/getNotionData.js
+++ src/getNotionData.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from 'react-router-dom';
+  const navigate = useNavigate();
 
 const GetNotionData = () => {
     const [notionData, setNotionData] = useState([]);
