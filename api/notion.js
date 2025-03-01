10cb751584953d4a3a6977fed29956826bf65a6d
Jorge Gómez López <jorgelogomez@gmail.com>
2025-03-01 12:21:51 +0100

    adding API - Notion

--- api/notion.js
+++ api/notion.js
@@ -2,10 +2,10 @@ export default async function handler(req, res) {
     const { REACT_APP_NOTION_API_KEY, REACT_APP_NOTION_DATABASE_ID } = process.env;
   
     try {
-      const response = await fetch(`https://api.notion.com/v1/databases/${NOTION_DATABASE_ID}/query`, {
+      const response = await fetch(`https://api.notion.com/v1/databases/${REACT_APP_NOTION_DATABASE_ID}/query`, {
         method: "POST",
         headers: {
-          "Authorization": `Bearer ${NOTION_API_KEY}`,
+          "Authorization": `Bearer ${REACT_APP_NOTION_API_KEY}`,
           "Notion-Version": "2022-06-28",
           "Content-Type": "application/json",
         },
