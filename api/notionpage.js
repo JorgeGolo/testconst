486e06aa954cd42762ca15447ca629e6395b080e
Jorge Gómez López <jorgelogomez@gmail.com>
2025-03-04 10:01:48 +0100

    notion data 8

--- api/notionpage.js
+++ api/notionpage.js
@@ -4,8 +4,6 @@ export default async function handler(req, res) {
     const { REACT_APP_NOTION_API_KEY } = process.env;
     const { pageIds } = req.query;
 
-    console.log("pageIds:", pageIds); // Log pageIds
-
     if (!pageIds) {
         return res.status(400).json({ error: "Page IDs requeridos" });
     }
@@ -13,15 +11,11 @@ export default async function handler(req, res) {
     try {
         const idsArray = JSON.parse(pageIds);
 
-        console.log("idsArray:", idsArray); // Log idsArray
-
         if (!Array.isArray(idsArray)) {
             return res.status(400).json({ error: "Page IDs debe ser un array" });
         }
 
         const promises = idsArray.map(async (pageId) => {
-            console.log("Fetching page with ID:", pageId); // Log pageId
-
             const response = await fetch(`https://api.notion.com/v1/pages/${pageId}`, {
                 headers: {
                     "Authorization": `Bearer ${REACT_APP_NOTION_API_KEY}`,
@@ -41,7 +35,12 @@ export default async function handler(req, res) {
         const filteredResults = results.filter(result => result !== null);
 
         const pageNames = filteredResults.map(page => {
-            return page.properties.Nombre.text[0].plain_text;
+            try {
+                return page.properties.Nombre.text[0].plain_text;
+            } catch (error) {
+                console.error("Error al obtener el nombre de la página:", error);
+                return "Nombre no encontrado"; // O maneja el caso de otra manera
+            }
         });
 
         res.status(200).json(pageNames);
